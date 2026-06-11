ca4675b50f71987103b394953e0019049ab3c424
skinmalata <mesigotochukwu@gmail.com>
2026-06-11 04:04:38 +0100

    Fix logout redirect to use url() helper with flash message

--- auth/logout.php
+++ auth/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
+require_once __DIR__ . '/../includes/functions.php';
 session_destroy();
-header('Location: /index.php');
-exit;
+redirectWith('/index.php', 'You have been logged out.');
